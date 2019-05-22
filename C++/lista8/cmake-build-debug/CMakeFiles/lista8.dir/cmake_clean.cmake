6df5064aded77c8cc7dfefb2a67903a0a0867c8e
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-05-22 21:41:30 +0200

    bst

--- C++/lista8/cmake-build-debug/CMakeFiles/lista8.dir/cmake_clean.cmake
+++ C++/lista8/cmake-build-debug/CMakeFiles/lista8.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/lista8.dir/main.cpp.obj"
+  "CMakeFiles/lista8.dir/main.cpp.o"
   "lista8.pdb"
-  "lista8.exe"
-  "lista8.exe.manifest"
-  "liblista8.dll.a"
+  "lista8"
 )
 
 # Per-language clean rules from dependency scanning.
