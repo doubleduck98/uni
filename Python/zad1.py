b88acb061fc1bdd7d13693cc3ecf6fc23f345b6f
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-10-02 15:58:39 +0200

    pythonik

---
+++ Python/zad1.py
@@ -0,0 +1,15 @@
+zakupy = [0.2, 0.5, 4.59, 6]
+
+def vat_faktura(lista):
+    suma = .23 * sum(lista)
+    print(suma)
+    return suma
+
+def vat_paragon(lista):
+    suma = 0.0
+    for x in lista:
+        suma += .23 * x
+    print(suma)
+    return suma
+
+print(vat_faktura(zakupy) == vat_paragon(zakupy))
