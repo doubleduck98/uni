74562054807a87f8ec97ae163c1776e92f9dbc53
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-10-16 12:04:27 +0200

    weppo

--- weppo/lista1/zad2.js
+++ weppo/lista1/zad2.js
@@ -1,24 +1,24 @@
-function przez_sume(num){
+function przez_sume(num) {
     var suma = 0;
     cyfry = Array.from(num.toString()).map(Number);
-    for(var i = 0; i < cyfry.length; i++){
+    for (var i = 0; i < cyfry.length; i++) {
         suma += cyfry[i];
     }
     return num % suma == 0;
 }
 
-function przez_kazda(num){
+function przez_kazda(num) {
     cyfry = Array.from(num.toString()).map(Number);
-    for(var i = 0; i < cyfry.length; i++){
-        if(num % cyfry[i] != 0) return false;
+    for (var i = 0; i < cyfry.length; i++) {
+        if (num % cyfry[i] != 0) return false;
     }
     return true;
 }
 
-function fn2(){
+function fn2() {
     res = [];
-    for(n = 1; n <= 100000; n++){
-        if(przez_sume(n) && przez_kazda(n))
+    for (n = 1; n <= 100000; n++) {
+        if (przez_sume(n) && przez_kazda(n))
             res.push(n);
     }
     console.log(res);
--- weppo/lista1/zad5.js
+++ weppo/lista1/zad5.js
@@ -1,32 +1,34 @@
-function fib_rek(num){
-    if(num === 0) return 0;
-    else if(num === 1) return 1;
-    else return fib_rek(num-1) + fib_rek(num-2);
+function fib_rek(num) {
+    if (num === 0) return 0;
+    else if (num === 1) return 1;
+    else return fib_rek(num - 1) + fib_rek(num - 2);
 }
 
-function fib_iter(num){
-    nums = [num+1];
+function fib_iter(num) {
+    var nums = new Array(num + 1);
     nums[0] = 0;
     nums[1] = 1;
-    for(var i = 2; i <= nums; i++){
-        nums[i] = nums[i-1] + nums[i-2];
+    for (var i = 2; i <= num; i++) {
+        nums[i] = nums[i - 1] + nums[i - 2];
     }
     return nums[num];
 }
 
-function test_rek(n){    
+function test_rek(n) {
     console.time("rek");
-    console.log("test rek: " +i +", " +fib_rek(n));
+    console.log("test rek: " + i + ", " + fib_rek(n));
     console.timeEnd("rek");
 }
 
-function fn5(){
-    for(var i = 10; i <= 40; i++){
+function fn5() {
+    for (var i = 10; i <= 40; i++) {
         console.time("iter");
-        console.log("iter: " +i +", " +fib_iter(i));
+        console.log("iter: " + i + ", " + fib_iter(i));
         console.timeEnd("iter");
         console.time("rek");
-        console.log("rek: " +i +", " +fib_rek(i));
+        console.log("rek: " + i + ", " + fib_rek(i));
         console.timeEnd("rek");
     }
 }
+
+fn5()
