d94ec40586e8ef4331e4adc19945d7fa291cfdf3
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-10-23 16:15:40 +0200

    weppo

---
+++ weppo/lista2/zad2.js
@@ -0,0 +1,17 @@
+autko = {
+    marka: 'aston martin',
+    model: 'db9',
+    'rok produkcji': '2012',
+    kolor: 'szary',
+    silnik: {
+        moc: '475',
+        rodzaj: 'V6'
+    }
+}
+
+console.log(autko.kolor);
+console.log(autko['kolor']);
+console.log(autko['rok produkcji']);
+
+console.log(autko[3]);
+console.log(autko['silnik']);
