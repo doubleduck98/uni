1075af0af18aed1756f51c48e224521e47510af7
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-10-28 14:14:35 +0100

    weppo

--- weppo/lista2/zad2.js
+++ weppo/lista2/zad2.js
@@ -6,7 +6,12 @@ autko = {
     silnik: {
         moc: '475',
         rodzaj: 'V6'
-    }
+    },
+    nic: 'dasdasd'
+}
+
+nic = {
+    nic: 'nic'
 }
 
 console.log(autko.kolor);
@@ -14,4 +19,17 @@ console.log(autko['kolor']);
 console.log(autko['rok produkcji']);
 
 console.log(autko[3]);
-console.log(autko['silnik']);
+console.log(autko[nic]);
+
+
+tablica = ['szymon', 'zienkiewicz', 16, 11, 1998];
+
+console.log(tablica[3]);
+console.log(tablica['3']);
+console.log(tablica.length)
+tablica.length = 2;
+console.log(tablica[1])
+console.log(tablica.length)
+tablica.length = 8;
+console.log(tablica.length)
+console.log(tablica[1])
