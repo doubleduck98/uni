3ac7313b2113d91ee6a1696cc1257fe81957c6ba
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-11-01 20:19:01 +0100

    pythonik

--- python/lista5/zad1.py
+++ python/lista5/zad1.py
@@ -1,5 +1,5 @@
 import timeit
-import subprocess as sub
+import subprocess
 
 
 def czy_pierwsza(n):
@@ -21,6 +21,11 @@ def pierwsze():
             i += 2
 
 
+def pierwsze_generator(n):
+    p = pierwsze()
+    return [p.__next__() for _ in range(1, n+1)]
+
+
 def pierwsze_imperatywna(n):
     pierwsze = []
     for i in range(2, n+1):
@@ -37,51 +42,24 @@ def pierwsze_funkcyjna(n):
     return list(filter(lambda x: czy_pierwsza(x), range(2, n+1)))
 
 
-def pierwsze_generator(n):
-    p = pierwsze()
-    return [p.__next__() for _ in range(1, n+1)]
-
-
-print(timeit.Timer('pierwsze_imperatywna(50)',
-                   globals=globals()).timeit(50))
-print(timeit.Timer('pierwsze_skladana(50)',
-                   globals=globals()).timeit(50))
-print(timeit.Timer('pierwsze_funkcyjna(50)',
-                   globals=globals()).timeit(50))
-print(timeit.Timer('pierwsze_generator(15)',
-                   globals=globals()).timeit(50))
-
-
 def gnuplot(punkty):
     def maxy_dla_generatora():
         return [len(pierwsze_skladana(n)) for n in punkty]
 
-    def timer_imp(n):
-        return str(timeit.Timer(f'pierwsze_imperatywna({n})',
-                                globals=globals()).timeit(1))
-
-    def timer_fun(n):
-        return str(timeit.Timer(f'pierwsze_funkcyjna({n})',
-                                globals=globals()).timeit(1))
-
-    def timer_skl(n):
-        return str(timeit.Timer(f'pierwsze_skladana({n})',
-                                globals=globals()).timeit(1))
-
-    def timer_gen(n):
-        return str(timeit.Timer(f'pierwsze_generator({n})',
-                                globals=globals()).timeit(1))
+    def timer(n, fun):
+        return str(timeit.Timer(f'{fun}({n})',
+                                globals=globals()).timeit(10) / 10)
     gen = maxy_dla_generatora()
     dat = open("plot.dat", "w")
     dat.write("#\tX\tgenerator\timperatywna\tskladana\tfunkcyjna\n")
     for n in range(0, len(punkty)):
         dat.write(
-            f'{punkty[n]}\t{timer_gen(gen[n])}\t{timer_imp(punkty[n])}\t{timer_skl(punkty[n])}\t{timer_fun(punkty[n])}\n')
+            f'{punkty[n]}\t{timer(gen[n], "pierwsze_generator")}\t{timer(punkty[n], "pierwsze_imperatywna")}\t{timer(punkty[n], "pierwsze_skladana")}\t{timer(punkty[n], "pierwsze_funkcyjna")}\n')
     dat.close()
     file_optput = open('plot.png', 'w+')
-    sub.run(['gnuplot', 'plot.gp'], stdout=file_optput,
-            cwd=r'/home/szipi/uni/python/lista5/')
+    subprocess.run(['gnuplot', 'plot.gp'], stdout=file_optput,
+                   cwd=r'/home/szipi/uni/python/lista5/')
     file_optput.close()
 
 
-gnuplot([x for x in range(100, 4000, 200)])
+gnuplot([x for x in range(100, 1000, 100)])
