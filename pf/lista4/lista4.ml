e5a7756b041993d8166eb9ecec6969738bfd4a34
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-11-07 13:31:49 +0100

    pf

--- pf/lista4/lista4.ml
+++ pf/lista4/lista4.ml
@@ -1,6 +1,5 @@
 (* SZYMON ZIENKIEWICZ *)
 (* ZADANIE 1 *)
-
 type 'a bt = Empty | Node of 'a * 'a bt * 'a bt;;
 
 let t = Node(1, Node(2, Empty, Node(3, Empty, Empty)), Empty);;
@@ -15,10 +14,10 @@ sum t;;
 sum tt;;
 
 (* ZADANIE 2*)
-let rec foldBT f acc tree = 
+let rec foldBT f str tree = 
   match tree with
-    Node(v, left, right) -> f v (foldBT f acc left, foldBT f acc right)
-  | Empty -> acc;;
+    Node(v, left, right) -> f v (foldBT f str left, foldBT f str right)
+  | Empty -> str;;
 
 foldBT (fun x (l, r) -> x + l + r) 0 t;;
 foldBT (fun x (l, r) -> x + l + r) 0 tt;;
@@ -38,5 +37,21 @@ inorderBTfold tt;;
 
 (* ZADANIE 4 *)
 let mapBT f tree =
-  foldBT
+  foldBT (fun x (l, r) -> Node(f x, l, r)) Empty tree;;
 
+mapBT (fun v -> 2*v) (Node(2, Node (4, Empty, Node (6, Empty, Empty)), Empty));;
+mapBT (fun v -> 2*v) t;;
+mapBT (fun v -> 5) tt;;
+
+(* ZADANIE 5 *)
+type 'a tree = L of 'a | N of 'a tree * 'a tree;;
+
+let store tree =
+  let rec str = function
+      L(x), xs -> Some x::xs
+    | N(l, r), xs -> str(l, str(r, None::xs))
+  in str (tree, []);;
+
+let d = N(L(1), N(L(2), L(3)));;
+
+store d;;
