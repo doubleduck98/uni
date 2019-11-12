54090d901697d8ea1788bfbafec28c7fc1cbf027
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-11-12 18:54:01 +0100

    rust

--- rust/lista6/zad2/src/main.rs
+++ rust/lista6/zad2/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    println!("{}", dig_pow(89, 1));
+    println!("{}", dig_pow(3456789, 5));
 }
 
 fn dig_pow(n: i64, p: i32) -> i64 {
+    let nvec: Vec<i64> = n
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap() as i64)
+        .collect();
+    let mut exp: u32 = p as u32;
+    let i: i64 = nvec.iter().fold(0, |acc, x| {
+        exp += 1;
+        acc + x.pow(exp-1)
+    });
+    let hm = (i as f64) / (n as f64);
+    if hm.fract() != 0.0 {
+        return -1;
+    }
+    i / n
 }
--- rust/lista6/zad3/src/main.rs
+++ rust/lista6/zad3/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", print(5));
 }
 
 fn print(n: i32) -> Option<String> {
-    if n < 0 || n % 2 == 0 {
-        None
+    if n < 1 || n % 2 == 0 {
+        return None;
     }
-    
+    let mut res: String = String::new();
+    for i in 0..n {
+        let row: String;
+        if i <= n / 2 {
+            row = "*".repeat((i * 2 + 1) as usize);
+        } else {
+            row = "*".repeat(((n - i) * 2 - 1) as usize);
+        }
+        res.push_str(&" ".repeat(((n - row.len() as i32) / 2) as usize));
+        res.push_str(&row);
+        res.push_str(&"\n");
+    }
+    Some(res)
 }
