09e57e0dc9231fd203a493904c1962a2d5c00163
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-11-20 18:39:20 +0100

    pythonik

--- python/lista6/zad1.py
+++ python/lista6/zad1.py
@@ -7,12 +7,17 @@ import re
 def find_python(url):
     try:
         url_contents = urllib.request.urlopen(url).read()
+    except urllib.error.HTTPError as e:
+        print(f'{e} ({url})')
+        raise
     except:
-        print(f'nie mozna połączyć się ze stroną: {url}')
-        return 'błąd łączenia się ze stroną'
-    py = re.compile('python')
-    site = BeautifulSoup(url_contents, features='html.parser')
-    return len(py.findall(str(site.contents)))
+        print(f'nieznany błąd podczas łączenia się z {url}')
+        raise
+    finally:
+        # For example r'\bfoo\b' matches 'foo', 'foo.', '(foo)', 'bar foo baz' but not 'foobar' or 'foo3'
+        py = re.compile(r'\bpython\b', re.IGNORECASE)
+        site = BeautifulSoup(url_contents, features='html.parser')
+        return len(py.findall(str(site.getText)))
 
 
 def gather_urls(current_url, distance, urls):
@@ -21,33 +26,41 @@ def gather_urls(current_url, distance, urls):
 
     try:
         site_contents = urllib.request.urlopen(current_url).read()
-    except:
-        print(f'nie mozna połączyć się ze stroną: {current_url}')
+    except urllib.error.HTTPError as e:
+        print(f'{e} ({current_url})')
         return
-
-    urls.add(current_url)
-    for header in BeautifulSoup(site_contents, parse_only=SoupStrainer('a'), features='html.parser'):
-        if header.has_attr('href'):
-            if header['href'] != '#' and header['href'].strip() != '':
-                url = urljoin(current_url, str(header['href']))
-                if url not in urls:
-                    urls.add(url)
-                    print(url)
-                    gather_urls(url, distance-1, urls)
-    
-    return urls
+    except:
+        print(f'nieznany błąd podczas łączenia się z {current_url}')
+        return
+    finally:
+        urls.add(current_url)
+        for header in BeautifulSoup(site_contents, parse_only=SoupStrainer('a'), features='html.parser'):
+            if header.has_attr('href'):
+                if not str(header['href']).startswith(r'\#') and header['href'].strip() != '':
+                    url = urljoin(current_url, str(header['href']))
+                    if url not in urls:
+                        urls.add(url)
+                        gather_urls(url, distance-1, urls)
 
 
 def crawl(base_url, distance, action):
 
     urls = set()
+    print('gathering URLs')
     gather_urls(base_url, distance, urls)
+    print(f'found {len(urls)} URLs')
 
     for url in urls:
-        yield (url, find_python(url))
+        try:
+            res = find_python(url)
+        except:
+            continue
+        finally:
+            yield (url, res)
 
 
-strona_startowa = 'https://pl.wikipedia.org/wiki/Specjalna:Losowa_strona'
+strona_startowa = 'https://pl.wikipedia.org/wiki/Python'
+
 
 for x in crawl(strona_startowa, 2, find_python):
     print(x)
