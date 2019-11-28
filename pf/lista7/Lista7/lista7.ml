b10635d8db13bc94595193ffd5dcf6612a956f74
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-11-28 12:41:37 +0100

    pf

--- pf/lista7/Lista7/lista7.ml
+++ pf/lista7/Lista7/lista7.ml
@@ -21,50 +21,91 @@ module type ORDER =
     val compare: t -> t -> ordering
   end;;
 
-module Comp : ORDER =
+module StringOrder : ORDER with type t = string =
     struct
-      type t
+      type t = string
+
+      let compare a b =
+        if a < b then  LT
+        else if a = b then EQ
+        else GT
+    end;;
+
+module IntOrder : ORDER with type t = int =
+    struct
+      type t = int
+
+      let compare a b =
+        if a < b then  LT
+        else if a <> b then EQ
+        else GT
+    end;;
+    
+module FloatOrder : ORDER with type t = float =
+    struct
+      type t = float
+
+      let compare a b =
+        if a < b then  LT
+        else if a = b then EQ
+        else GT
+    end;;
+
+module CharOrder : ORDER with type t = char =
+    struct
+      type t = char
+
+      let compare a b =
+        if a < b then  LT
+        else if a = b then EQ
+        else GT
+    end;;
+
 
 module Slownik(Key: ORDER) : DICTIONARY with type key = Key.t =
-    struct
-      type key = Key.t
+  struct
+    type key = Key.t
 
-      type 'a t = (key * 'a) list
+    type 'a t = (key * 'a) list
 
-      exception DuplicatedKey of key
+    exception DuplicatedKey of key
 
-      let empty() = []
+    let empty() = []
 
-      let rec lookup dict key =
-        match (dict, key) with
-          ([], sk) -> None
-        | (k, v)::d, sk ->
-          if k = sk then Some v
-          else lookup (d, sk)
+    let rec lookup dict key =
+      match (dict, key) with
+        ([], sk) -> None
+      | (k, v)::d, sk ->
+        if k = sk then Some v
+        else lookup d sk
 
-      let insert dict new_pair =
-        let comp p1 p2 =
-          if fst p1 < fst p2
-            then -1
-          else if fst p1 = fst p2
-            then 0
-          else 1 in
-        let eq pair =
-          fst pair = fst new_pair in
-        if List.exists eq dict 
-          then raise (DuplicatedKey (fst new_pair))
-          else List.sort comp (new_pair :: dict)
+    let rec insert dict new_pair =
+      if dict = [] then [new_pair]
+      else
+        match Key.compare (fst(List.hd dict)) (fst(new_pair)) with
+            LT -> new_pair::dict
+          | GT -> (List.hd dict)::insert (List.tl dict) new_pair
+          | EQ -> raise (DuplicatedKey (fst new_pair))
 
-      let rec delete = function
-          ([], k) -> []
-        | ((k, v)::d, sk) -> 
-          if k = sk then d
-          else (k, v)::delete(d, k)
+    let rec delete dict key =
+      match (dict, key) with
+        ([], k) -> []
+      | ((k, v)::d, sk) -> 
+        if k = sk then d
+        else (k, v)::delete d sk
 
-      let rec update = function
-          ([], k) -> []
-        | ((k, v)::d, (sk, nv)) ->
-          if k = sk then (sk, nv)::d
-          else (k, v)::update(d, (sk, nv))
+    let rec update dict new_pair =
+      match (dict, new_pair) with
+        ([], k) -> []
+      | ((k, v)::d, (sk, nv)) ->
+        if k = sk then (sk, nv)::d
+        else (k, v)::update d new_pair
 
-    end;;
+  end;;
+
+module StringDict = Slownik(StringOrder);;
+let d = StringDict.empty();;
+let ( <| ) d (k,x) = StringDict.insert d (k,x);;
+
+let d = d <| ("jp", 997) <| ("essa", 4);;
+StringDict.lookup d "jp";;
