33dec9dfbdb6a665c8ab776be23b59acce180479
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-12-06 16:29:50 +0100

    rust

--- rust/lista9/zad1/src/main.rs
+++ rust/lista9/zad1/src/main.rs
@@ -1,3 +1,64 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", part_list(vec!["I", "wish", "I", "hadn't", "come"]));
+}
+
+fn part_list(arr: Vec<&str>) -> String {
+    let mut res: Vec<String> = Vec::new();
+    for i in 1..arr.len() {
+        res.push(format!("({}, {})", arr[..i].join(" "), arr[i..].join(" ")))
+    }
+    res.join("")
+}
+
+#[test]
+fn test0() {
+    assert_eq!(part_list(vec!["I", "wish", "I", "hadn't", "come"]),
+                "(I, wish I hadn't come)(I wish, I hadn't come)(I wish I, hadn't come)(I wish I hadn't, come)")
+}
+#[test]
+fn test1() {
+    assert_eq!(
+        part_list(vec!["cdIw", "tzIy", "xDu", "rThG"]),
+        "(cdIw, tzIy xDu rThG)(cdIw tzIy, xDu rThG)(cdIw tzIy xDu, rThG)"
+    )
+}
+#[test]
+fn test2() {
+    assert_eq!(
+        part_list(vec!["fnatic", "vs", "liquid"]),
+        "(fnatic, vs liquid)(fnatic vs, liquid)"
+    )
+}
+#[test]
+fn test3() {
+    assert_eq!(
+        part_list(vec!["Xd", "xd", "XD", "xD"]),
+        "(Xd, xd XD xD)(Xd xd, XD xD)(Xd xd XD, xD)"
+    )
+}
+#[test]
+fn test4() {
+    assert_eq!(part_list(vec!["dasd", "das d", "fasfasfa", "fasfafsa"]),
+                "(dasd, das d fasfasfa fasfafsa)(dasd das d, fasfasfa fasfafsa)(dasd das d fasfasfa, fasfafsa)")
+}
+#[test]
+fn test5() {
+    assert_eq!(part_list(vec!["sprzedam", "fmsa"]), "(sprzedam, fmsa)")
+}
+#[test]
+fn test6() {
+    assert_eq!(part_list(vec![""]), "")
+}
+#[test]
+fn test7() {
+    assert_eq!(part_list(vec!["lorem", "ipsum", "dolor", "sit", "amet"]),
+                "(lorem, ipsum dolor sit amet)(lorem ipsum, dolor sit amet)(lorem ipsum dolor, sit amet)(lorem ipsum dolor sit, amet)")
+}
+#[test]
+fn test8() {
+    assert_eq!(part_list(vec!["xd"]), "")
+}
+#[test]
+fn test9() {
+    assert_eq!(part_list(vec![]), "")
 }
--- rust/lista9/zad4/src/main.rs
+++ rust/lista9/zad4/src/main.rs
@@ -1,3 +1,61 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", camel_case("the quick brown fox jumped over lazy dog"));
+}
+
+fn camel_case(str: &str) -> String {
+    str.split_whitespace()
+        .map(|w| {
+            let mut chars: Vec<char> = w.chars().collect();
+            chars[0] = chars[0].to_uppercase().nth(0).unwrap();
+            chars.iter().collect::<String>()
+        })
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+#[test]
+fn test0() {
+    assert_eq!(camel_case("test case"), "TestCase");
+}
+#[test]
+fn test1() {
+    assert_eq!(camel_case("camel case method"), "CamelCaseMethod");
+}
+#[test]
+fn test2() {
+    assert_eq!(camel_case("say hello "), "SayHello");
+}
+#[test]
+fn test3() {
+    assert_eq!(camel_case(" camel case word"), "CamelCaseWord");
+}
+#[test]
+fn test4() {
+    assert_eq!(camel_case(""), "");
+}
+#[test]
+fn test5() {
+    assert_eq!(
+        camel_case("the quick brown fox jumped over lazy dog"),
+        "TheQuickBrownFoxJumpedOverLazyDog"
+    );
+}
+#[test]
+fn test6() {
+    assert_eq!(camel_case("x d d d d d   "), "XDDDDD");
+}
+#[test]
+fn test7() {
+    assert_eq!(camel_case("worki na smieci"), "WorkiNaSmieci");
+}
+#[test]
+fn test8() {
+    assert_eq!(camel_case("nervarien na allstars"), "NervarienNaAllstars");
+}
+#[test]
+fn test9() {
+    assert_eq!(
+        camel_case("i cant go to yemen im an analyst"),
+        "ICantGoToYemenImAnAnalyst"
+    );
 }
