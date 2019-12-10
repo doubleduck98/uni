f872b884ead37ad78fc06fdbfd5fba203f0d6c37
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2019-12-10 18:27:06 +0100

    rust

--- rust/lista10/zad2/src/main.rs
+++ rust/lista10/zad2/src/main.rs
@@ -10,33 +10,15 @@ fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
     if list_art.is_empty() || list_cat.is_empty() {
         return String::new();
     }
-    let mut arts: Vec<&str> = list_art;
     let mut map: HashMap<String, u32> = HashMap::new();
     let mut res: Vec<String> = Vec::new();
-    arts.retain(|art| {
+    for art in list_art {
         let art_type: String = art.chars().nth(0).unwrap().to_string();
-        list_cat.iter().any(|cat| *cat.to_string() == art_type)
-    });
-    list_cat.iter().for_each(|cat| {
-        map.entry(cat.to_string()).or_insert(0);
-    });
-    arts.iter().for_each(|art| {
-        let art_type: String = art.chars().nth(0).unwrap().to_string();
-        let quantity: u32 = art.split_whitespace().collect::<Vec<_>>()[1]
-            .to_string()
-            .parse()
-            .unwrap();
-        map.entry(art_type)
-            .and_modify(|q| *q += quantity)
-            .or_insert(0);
-    });
-    map.iter()
-        .for_each(|(k, v)| res.push(format!("({} : {})", k, v.to_string())));
-    res.sort_by_key(|s| {
-        list_cat
-            .iter()
-            .position(|&c| c.to_string().chars().nth(0).unwrap() == s.chars().nth(1).unwrap())
-            .unwrap()
-    });
+        let quantity: u32 = art.split_whitespace().next_back().unwrap().parse().unwrap();
+        *map.entry(art_type).or_insert(0) += quantity;
+    }
+    for cat in list_cat {
+        res.push(format!("({} : {})", cat, map.get(cat).unwrap_or(&0)))
+    }
     res.join(" - ")
 }
