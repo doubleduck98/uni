5ec08faaf1e621d264212ddb86038f4aeac514f7
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2020-05-17 18:39:47 +0200

    dotnet

---
+++ dotnet/lista7/zadanie2.3.3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace zadanie2._3._3
+{
+    internal class Program
+    {
+        private static string Haslo()
+        {
+            Console.WriteLine("podaj 8-znakowe hasło do zaszyfrowania:");
+            var passwd = Console.ReadLine();
+            if (passwd != null && passwd.Length != 8)
+            {
+                Console.WriteLine("hasło ma złą długość");
+                Environment.Exit(0);
+            }
+
+            return passwd;
+        }
+
+        public static void Main(string[] args)
+        {
+            var key = new UnicodeEncoding().GetBytes(Haslo());
+            
+            // szyfrowanie
+            using (var fsIn = new FileStream(Path.Combine(Environment.CurrentDirectory, "plik.txt"), FileMode.Open,
+                FileAccess.Read))
+            {
+                using (var crypt = new CryptoStream(fsIn, new AesManaged().CreateEncryptor(key, key),
+                    CryptoStreamMode.Read))
+                {
+                    using (var compressed = File.Create(fsIn.Name + ".gz"))
+                    {
+                        using (var gzip = new GZipStream(compressed, CompressionMode.Compress))
+                        {
+                            crypt.CopyTo(gzip);
+                        }
+                    }
+                }
+            }
+
+            // odszyfrowanie
+            using (var fsIn = new FileStream(Path.Combine(Environment.CurrentDirectory, "plik.txt.gz"), FileMode.Open,
+                FileAccess.Read))
+            {
+                using (var decrypt = new CryptoStream(Console.OpenStandardOutput(),
+                    new AesManaged().CreateDecryptor(key, key),
+                    CryptoStreamMode.Write))
+                {
+                    using (var gzip = new GZipStream(fsIn, CompressionMode.Decompress))
+                    {
+                        gzip.CopyTo(decrypt);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/lista7/zadanie2.3.4/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading;
+
+namespace zadanie2._3._4
+{
+    internal class Program
+    {
+        private static Semaphore _tyton;
+        private static Semaphore _papier;
+        private static Semaphore _zapalki;
+        private static Semaphore _koniecPalenia;
+        private static Semaphore _palaczTyton;
+        private static Semaphore _palaczPapier;
+        private static Semaphore _palaczZapalki;
+        private static Mutex _mutex;
+        private static bool _papierDostepny;
+        private static bool _tytonDostepny;
+        private static bool _zapalkiDostepne;
+
+        private static void Agent()
+        {
+            var rand = new Random();
+            while (true)
+            {
+                _koniecPalenia.WaitOne();
+                var wybor = rand.Next(1, 4) % 3;
+                // dajemy papier i tytoń
+                if (wybor == 0)
+                {
+                    _tyton.Release(1);
+                    _papier.Release(1);
+                }
+                // dajemy papier i zapałki
+                else if (wybor == 1)
+                {
+                    _zapalki.Release(1);
+                    _papier.Release(1);
+                }
+                // dajemy zapałki i tytoń
+                else
+                {
+                    _zapalki.Release(1);
+                    _tyton.Release(1);
+                }
+            }
+        }
+
+        private static void PalaczZZapalkami()
+        {
+            while (true)
+            {
+                _palaczZapalki.WaitOne();
+                Zapal("Palacz z zapałkami");
+                _koniecPalenia.Release(1);
+            }
+        }
+
+        private static void PodawaczZap()
+        {
+            while (true)
+            {
+                _zapalki.WaitOne();
+                _mutex.WaitOne();
+                if (_papierDostepny)
+                {
+                    _papierDostepny = false;
+                    _palaczTyton.Release(1);
+                }
+                else if (_tytonDostepny)
+                {
+                    _tytonDostepny = false;
+                    _palaczPapier.Release(1);
+                }
+                else
+                {
+                    _zapalkiDostepne = true;
+                }
+
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        private static void PalaczZTytoniem()
+        {
+            while (true)
+            {
+                _palaczTyton.WaitOne();
+                Zapal("Palacz z tytoniem");
+                _koniecPalenia.Release(1);
+            }
+        }
+
+        private static void PodawaczTyt()
+        {
+            while (true)
+            {
+                _tyton.WaitOne();
+                _mutex.WaitOne();
+                if (_papierDostepny)
+                {
+                    _papierDostepny = false;
+                    _palaczZapalki.Release(1);
+                }
+                else if (_zapalkiDostepne)
+                {
+                    _zapalkiDostepne = false;
+                    _palaczPapier.Release(1);
+                }
+                else
+                {
+                    _tytonDostepny = true;
+                }
+
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        private static void PalaczZPapierem()
+        {
+            while (true)
+            {
+                _palaczPapier.WaitOne();
+                Zapal("Palacz z papierem");
+                _koniecPalenia.Release(1);
+            }
+        }
+
+        private static void PodawaczPap()
+        {
+            while (true)
+            {
+                _papier.WaitOne();
+                _mutex.WaitOne();
+                if (_tytonDostepny)
+                {
+                    _tytonDostepny = false;
+                    _palaczZapalki.Release(1);
+                }
+                else if (_zapalkiDostepne)
+                {
+                    _zapalkiDostepne = false;
+                    _palaczTyton.Release(1);
+                }
+                else
+                {
+                    _papierDostepny = true;
+                }
+
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        private static void Zapal(string kto)
+        {
+            Console.WriteLine(kto + " skręca i zapala papierosa.");
+            Thread.Sleep(500);
+        }
+
+        private static void Init()
+        {
+            _tyton = new Semaphore(0, 1);
+            _palaczTyton = new Semaphore(0, 1);
+            _papier = new Semaphore(0, 1);
+            _palaczPapier = new Semaphore(0, 1);
+            _zapalki = new Semaphore(0, 1);
+            _palaczZapalki = new Semaphore(0, 1);
+            _koniecPalenia = new Semaphore(1, 1);
+            _mutex = new Mutex();
+            _papierDostepny = _zapalkiDostepne = _tytonDostepny = false;
+        }
+
+        private static void StartAndWait()
+        {
+            var agent = new Thread(Agent) {IsBackground = true};
+            var palaczT = new Thread(PalaczZTytoniem) {IsBackground = true};
+            var palaczZ = new Thread(PalaczZZapalkami) {IsBackground = true};
+            var palaczP = new Thread(PalaczZPapierem) {IsBackground = true};
+            var pusherT = new Thread(PodawaczTyt) {IsBackground = true};
+            var pusherZ = new Thread(PodawaczZap) {IsBackground = true};
+            var pusherP = new Thread(PodawaczPap) {IsBackground = true};
+            palaczT.Start();
+            palaczZ.Start();
+            palaczP.Start();
+            pusherT.Start();
+            pusherZ.Start();
+            pusherP.Start();
+            agent.Start();
+            agent.Join();
+            palaczT.Join();
+            palaczZ.Join();
+            palaczP.Join();
+            pusherT.Join();
+            pusherZ.Join();
+            pusherP.Join();
+        }
+        
+        public static void Main(string[] args)
+        {
+            Init();
+            StartAndWait();
+        }
+    }
+}
