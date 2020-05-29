2ccc5f04037d78fc9ff50886f3210e7aeb92c66f
doubleduck98 <szymon.zienkiewicz98@gmail.com>
2020-05-29 15:53:04 +0200

    dotnet

---
+++ dotnet/lista9/zadanie2.4.2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace zadanie2._4._2
+{
+    internal static class Program
+    {
+        private static void ValidationHandler(object sender, ValidationEventArgs args)
+        {
+            Console.WriteLine("Błąd walidacji!");
+            Console.WriteLine("\tWaznosc: {0}", args.Severity);
+            Console.WriteLine("\tInfo: {0}", args.Message);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwszy dokument:");
+            var xml = new XmlDocument();
+            var tr = new XmlTextReader("ListaStudentow.xml");
+            var reader = new XmlValidatingReader(tr) {ValidationType = ValidationType.Schema};
+            reader.ValidationEventHandler += ValidationHandler;
+            xml.Load(reader);
+            Console.WriteLine("Dokument poprawny!");
+            Console.WriteLine("***");
+            Console.WriteLine("Drugi dokument:");
+            tr = new XmlTextReader("ZlaListaStudentow.xml");
+            reader = new XmlValidatingReader(tr) {ValidationType = ValidationType.Schema};
+            reader.ValidationEventHandler += ValidationHandler;
+            xml.Load(reader);
+        }
+    }
+}
---
+++ dotnet/lista9/zadanie2.4.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace zadanie2._4._4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var xmls = new XmlSerializer(typeof(ListaStudentowType));
+            var fs = new FileStream("ListaStudentow.xml", FileMode.Open);
+            var listaStudentow = (ListaStudentowType) xmls.Deserialize(fs);
+            foreach (var student in listaStudentow.Student)
+            {
+                Console.WriteLine($"{student.Imie} {student.Nazwisko}");
+                Console.Write($"{student.Adres.Ulica} {student.Adres.NrDomu}");
+                if (student.Adres.MieszkanieSpecified)
+                {
+                    Console.WriteLine($"/{student.Adres.Mieszkanie}");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine($"{student.Adres.KodPocztowy}, {student.Adres.Miasto}");
+            }
+        }
+    }
+}
